733b279c680772824db1722ca0028b53e5609d41
narmanb <43427228+narmanb@users.noreply.github.com>
2026-09-20 20:35:50 -0500

    Add Android file picker export integration

---
+++ addons/AndroidFilePicker/export_plugin.gd
@@ -0,0 +1,26 @@
+@tool
+extends EditorPlugin
+
+var export_plugin: AndroidExportPlugin
+
+func _enter_tree() -> void:
+	export_plugin = AndroidExportPlugin.new()
+	add_export_plugin(export_plugin)
+
+func _exit_tree() -> void:
+	remove_export_plugin(export_plugin)
+	export_plugin = null
+
+class AndroidExportPlugin extends EditorExportPlugin:
+	var _plugin_name := "AndroidFilePicker"
+
+	func _supports_platform(platform) -> bool:
+		return platform is EditorExportPlatformAndroid
+
+	func _get_android_libraries(_platform, debug: bool) -> PackedStringArray:
+		if debug:
+			return PackedStringArray(["AndroidFilePicker/app-debug.aar"])
+		return PackedStringArray(["AndroidFilePicker/app-release.aar"])
+
+	func _get_name() -> String:
+		return _plugin_name
